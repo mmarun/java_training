e75adc444da5b661e08f3c298bd88cde8ff8dae1
mmarun <mmarun585@gmail.com>
2021-07-13 21:59:02 +0530

    "Intellij intro"

---
+++ Session 4/application/src/org/javatutorials/Main.java
@@ -0,0 +1,10 @@
+package org.javatutorials;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Arun";
+        System.out.println("Hello - " + name);
+    }
+}
