93a416910b1b7eb4b42ec0691263aa40b7dc01e6
amruthasudheesh <amrutha.sudheesh.k@gmail.com>
2021-07-18 07:01:17 +0530

    Amrutha code updated

---
+++ Students/Amrutha/session7/mavenDemo/mavenDemo/src/main/java/org/demo/MathHelper.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+/**
+ * class for performing remainder functionality
+ */
+public class MathHelper {
+    /**
+     * remainder method to find remainder of number by modNumber
+     * @param number
+     * @param modNumber
+     * @return remainder of integer number by modNumber
+     */
+    public int remainder(int number,int modNumber)
+    {
+        return number%modNumber;
+    }
+}
