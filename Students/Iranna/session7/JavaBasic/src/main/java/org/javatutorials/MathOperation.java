d28804ae575deadae8b3fc48f83659bf2a41f21d
irannagkk <hejjegariranna@gmail.com>
2021-07-18 08:53:47 +0530

    iranna java basic assignments

---
+++ Students/Iranna/session7/JavaBasic/src/main/java/org/javatutorials/MathOperation.java
@@ -0,0 +1,48 @@
+package org.javatutorials;
+
+public class MathOperation {
+    /**
+     * return addition of two numbers
+     * @param a
+     * @param b
+     * @return sum of two int
+     */
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+
+    /**
+     * return subtration  of two numbers
+     * @param a
+     * @param b
+     * @return subtractin of two int
+     */
+    public int subtract(int a,int b)
+    {
+        return a-b;
+    }
+
+    /**
+     * return multiplication  of two numbers
+     * @param a
+     * @param b
+     * @return multiplication of two int
+     */
+    public int multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+    /**
+     * return division  of two numbers
+     * @param a
+     * @param b
+     * @return division of two int
+     */
+
+    public int divide(int a,int b)
+    {
+        return a/b;
+    }
+}
