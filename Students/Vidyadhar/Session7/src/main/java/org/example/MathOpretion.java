481b86e5644c777ae77c8f2a97fa20b0dcbdb1e8
VidyadharBP <vidyadharpirannvar@gmail.com>
2021-07-19 20:12:09 +0530

    session 7 assignment

---
+++ Students/Vidyadhar/Session7/src/main/java/org/example/MathOpretion.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class MathOpretion {
+
+
+    /**Addtion of two number
+     *
+     * @param a
+     * @param b
+     * @return a+b
+     */
+    public int add(int a,int b)
+    {
+        return a+b;
+    }
+
+
+    /** substraction of two number
+     *
+     * @param c
+     * @param d
+     * @return c-d
+     */
+
+    public int substract(int c,int d)
+    {
+        return c-d;
+    }
+
+
+
+    /** Multiplication of two number
+     *
+     * @param n1
+     * @param n2
+     * @return n1*n2
+     */
+    public int multiply(int n1,int n2)
+    {
+        return n1*n2;
+    }
+
+
+
+    /** Division of two number
+     *
+     * @param n3
+     * @param n4
+     * @return n3/n4
+     */
+    public int divide(int n3,int n4)
+    {
+        return n3/n4;
+    }
+
+}
