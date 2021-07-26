db0e7afb6e20fdd22879410688d4a5994e730baa
VidyadharBP <vidyadharpirannvar@gmail.com>
2021-07-26 13:37:28 +0530

    issuse solved

--- Students/Vidyadhar/Session7/src/main/java/org/example/App.java
+++ Students/Vidyadhar/Session7/src/main/java/org/example/App.java
@@ -5,17 +5,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        MathOpretion M1=new MathOpretion();
-        MathHelper M2=new MathHelper();
+        MathOperation mathOperation=new MathOperation();
+        MathHelper mathHelper=new MathHelper();
 
-        System.out.println("the addition of two number"+M1.add(25,20));
+        System.out.println("the addition of two number"+mathOperation.add(25,20));
 
-        System.out.println("the addition of two number"+M1.substract(25,20));
+        System.out.println("the addition of two number"+mathOperation.substract(25,20));
 
-        System.out.println("multiplication of two number" +M1.multiply(15,15));
+        System.out.println("multiplication of two number" +mathOperation.multiply(15,15));
 
-        System.out.println("division of two number" +M1.divide(25,5));
+        System.out.println("division of two number" +mathOperation.divide(25,5));
 
-        System.out.println("the reminder of two number" +M2.reminder(25,7));
+        System.out.println("the reminder of two number" +mathHelper.reminder(25,7));
     }
 }
