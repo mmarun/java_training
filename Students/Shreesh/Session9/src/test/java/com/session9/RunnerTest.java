06d47d45d970497673e0c42ef63698d260814b03
joshishreesh <shreesh.22599@gmail.com>
2021-07-31 20:09:14 +0530

    Session 9 and Session 14 Assginment, Session 13 Includes session 12 assignment

---
+++ Students/Shreesh/Session9/src/test/java/com/session9/RunnerTest.java
@@ -0,0 +1,13 @@
+package com.session9;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RunnerTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue(true);
+    }
+}
---
+++ Students/Shreesh/Sesson14/src/test/java/com/hashMap/RunnerTest.java
@@ -0,0 +1,14 @@
+package com.hashMap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RunnerTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
