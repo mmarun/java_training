3c3be52ddec446d4c6d0f3c03a70fae61889958d
amruthasudheesh <amrutha.sudheesh.k@gmail.com>
2021-08-02 07:41:35 +0530

    updated stack

---
+++ Students/Amrutha/session11/mavenDemo/src/main/java/org/demo/StackOperations.java
@@ -0,0 +1,8 @@
+package org.demo;
+
+public interface StackOperations<T> {
+public void push(T data);
+public void pop();
+public T peek();
+public void printList();
+}
---
+++ Students/Amrutha/session11/mavenDemo/src/test/java/org/demo/StackTest.java
@@ -0,0 +1,48 @@
+package org.demo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+public class StackTest<T> {
+    Stack<Integer> stackTest;
+
+    @Before
+    public void setUp()  {
+        stackTest=new Stack<>(5);
+        stackTest.push(1);
+        stackTest.push(2);
+        stackTest.push(3);
+    }
+
+    @Test
+    public void pushTest()
+    {
+        Assert.assertEquals(Integer.valueOf(3),stackTest.peek() );
+
+    }
+    @Test(expected = NullPointerException.class)
+    public void negativepushTest()
+    {
+        stackTest=null;
+        stackTest.push(1);
+
+    }
+    @Test
+    public void popTest()
+    {
+        Assert.assertEquals(Integer.valueOf(3),stackTest.peek() );
+        stackTest.pop();
+        Assert.assertEquals(Integer.valueOf(2),stackTest.peek() );
+       
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void negativepopTest()
+    {
+        stackTest=null;
+        stackTest.pop();
+    }
+}
